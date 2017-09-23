7504dffad1ce6fc946f2b3971b0c143df5e2a0dc
vanessa <vmariesainte@gmail.com>
2017-09-23 11:01:00 +0200

    structure statique du blog de départ

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8">
+		<title>Blog php</title>
+		<link rel="stylesheet" href="reset.css">
+		<link rel="stylesheet" href="main.css">
+	</head>
+
+	<body>
+		<div class="wrapper">
+			<div class="articles-list">
+				<article>
+					<h1>Mon super titre</h1>
+					<div class="meta-data">
+						<span class="category">Catégorie: ma catégorie</span>
+						<span class="creation-date">Date: 23/09/2017</span>
+						<span class="author">Auteur: Vanessa</span>
+					</div>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est fuga magni, cupiditate odio porro velit architecto soluta quas non tempora laborum tempore voluptas, a, accusantium aperiam odit dignissimos nihil id?
+					</p>
+				</article>
+			</div>
+		</div>
+	</body>
+</html>
